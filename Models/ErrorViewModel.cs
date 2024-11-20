07437da1d716864bdcfb90489de90354e9654a3f
Matex2395 <mateorgafjo@gmail.com>
2024-11-19 23:33:14 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskTimePredicter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
