271c79706a386fbe534f0deade142a88759e54fe
Matex2395 <mateorgafjo@gmail.com>
2024-11-20 18:32:31 -0500

    Controladores - Categoría y Tarea

---
+++ Controllers/QuestsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TaskTimePredicter.Data;
+using TaskTimePredicter.Models;
+
+namespace TaskTimePredicter.Controllers
+{
+    public class QuestsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public QuestsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Quests
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Quests.Include(q => q.Category).Include(q => q.User);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Quests/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quest = await _context.Quests
+                .Include(q => q.Category)
+                .Include(q => q.User)
+                .FirstOrDefaultAsync(m => m.QuestId == id);
+            if (quest == null)
+            {
+                return NotFound();
+            }
+
+            return View(quest);
+        }
+
+        // GET: Quests/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserEmail");
+            return View();
+        }
+
+        // POST: Quests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("QuestId,QuestName,EstimatedTime,ActualTime,QuestState,CreationDate,UserId,CategoryId")] Quest quest)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(quest);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName", quest.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserEmail", quest.UserId);
+            return View(quest);
+        }
+
+        // GET: Quests/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quest = await _context.Quests.FindAsync(id);
+            if (quest == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName", quest.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserEmail", quest.UserId);
+            return View(quest);
+        }
+
+        // POST: Quests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("QuestId,QuestName,EstimatedTime,ActualTime,QuestState,CreationDate,UserId,CategoryId")] Quest quest)
+        {
+            if (id != quest.QuestId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(quest);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuestExists(quest.QuestId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryName", quest.CategoryId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserEmail", quest.UserId);
+            return View(quest);
+        }
+
+        // GET: Quests/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var quest = await _context.Quests
+                .Include(q => q.Category)
+                .Include(q => q.User)
+                .FirstOrDefaultAsync(m => m.QuestId == id);
+            if (quest == null)
+            {
+                return NotFound();
+            }
+
+            return View(quest);
+        }
+
+        // POST: Quests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var quest = await _context.Quests.FindAsync(id);
+            if (quest != null)
+            {
+                _context.Quests.Remove(quest);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuestExists(int id)
+        {
+            return _context.Quests.Any(e => e.QuestId == id);
+        }
+    }
+}
