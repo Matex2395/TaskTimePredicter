77278f8f3adcbeef13e095913763f7ffd6f6742b
Matex2395 <mateorgafjo@gmail.com>
2024-11-20 15:36:59 -0500

    AppDbContext - Connection String - Ajustes a los Modelos

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TaskTimePredicter.Models;
+
+namespace TaskTimePredicter.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options) : base(options) { }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Quest> Quests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Tabla "Users"
+            modelBuilder.Entity<User>(
+                entity =>
+                {
+                    entity.HasKey(e => e.UserId).HasName("PK__Users__CB9A1CFF1714BBE7");
+                    entity.HasIndex(e => e.UserEmail, "UQ__Users__D54ADF555E66784B").IsUnique();
+                    entity.Property(e => e.UserId).HasColumnName("userId");
+                    entity.Property(e => e.CreatedAt)
+                        .HasDefaultValueSql("(getdate())")
+                        .HasColumnName("createdAt");
+                    entity.Property(e => e.UserEmail)
+                        .HasMaxLength(100)
+                        .HasColumnName("userEmail");
+                    entity.Property(e => e.UserName)
+                        .HasMaxLength(100)
+                        .HasColumnName("userName");
+                    entity.Property(e => e.UserPassword)
+                        .HasMaxLength(100)
+                        .HasColumnName("userPassword");
+                    entity.Property(e => e.UserRole)
+                        .HasMaxLength(50)
+                        .HasDefaultValue("Developer")
+                        .HasColumnName("userRole");
+                    entity.HasData(new User
+                    {
+                        UserId = 001,
+                        UserName = "Administrador Base",
+                        UserPassword = "admin123",
+                        UserRole = "Administrator",
+                        UserEmail = "admin@gmail.com",
+                        CreatedAt = DateOnly.FromDateTime(DateTime.Now)
+                    });
+                });
+
+            //Tabla "Categories"
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId).HasName("PK__Categori__23CAF1D81A434EC4");
+
+                entity.Property(e => e.CategoryId).HasColumnName("categoryId");
+                entity.Property(e => e.CategoryDescription)
+                    .HasMaxLength(255)
+                    .HasColumnName("categoryDescription");
+                entity.Property(e => e.CategoryName)
+                    .HasMaxLength(100)
+                    .HasColumnName("categoryName");
+            });
+
+            //Tabla "Quests"
+            modelBuilder.Entity<Quest>(entity =>
+            {
+                entity.HasKey(e => e.QuestId);
+
+                entity.Property(e => e.QuestId).HasColumnName("taskId");
+                entity.Property(e => e.ActualTime).HasColumnName("actualTime");
+                entity.Property(e => e.CategoryId).HasColumnName("categoryId");
+                entity.Property(e => e.CreationDate)
+                    .HasDefaultValueSql("(getdate())")
+                    .HasColumnName("creationDate");
+                entity.Property(e => e.EstimatedTime).HasColumnName("estimatedTime");
+                entity.Property(e => e.QuestName)
+                    .HasMaxLength(100)
+                    .HasColumnName("questName");
+                entity.Property(e => e.QuestState)
+                    .HasMaxLength(50)
+                    .HasDefaultValue("In Progress")
+                    .HasColumnName("questState");
+                entity.Property(e =>  e.UserId).HasColumnName("userId");
+                //Relaciones FK
+                entity.HasOne(d => d.User)
+                    .WithMany()
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.SetNull);
+                entity.HasOne(d => d.Category)
+                    .WithMany()
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.SetNull);
+            });
+        }
+    }
+}
--- Models/Quest.cs
+++ Models/Quest.cs
@@ -3,23 +3,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TaskTimePredicter.Models
 {
-    public class Task
+    public class Quest
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int TaskId { get; set; }
+        public int QuestId { get; set; }
         [Required]
-        public required string TaskName { get; set; }
+        public required string QuestName { get; set; }
         [Required]
         public double EstimatedTime { get; set; }
         public double? ActualTime { get; set; }
         [Required]
-        public required string TaskState { get; set; }
+        public required string QuestState { get; set; }
         public DateOnly CreationDate { get; set; }
         
         public int? UserId { get; set; }
-        public User? User { get; set; }
+        public virtual User? User { get; set; }
         public int? CategoryId { get; set; }
-        public Category? Category { get; set; }
+        public virtual Category? Category { get; set; }
     }
 }
