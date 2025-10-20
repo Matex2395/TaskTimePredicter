df47655c66ac6d6ff1d4e0dc2527e5cc1538dc23
Matex2395 <mateorgafjo@gmail.com>
2025-10-20 00:09:02 -0500

    Agregar Tests

---
+++ Tests/SampleTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace TaskTimePredicter.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void SimpleAdditionTest()
+        {
+            int a = 1;
+            int b = 1;
+            int result = a + b;
+
+            Assert.Equal(2, result);
+        }
+    }
+}
